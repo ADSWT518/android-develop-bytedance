7208566c916c8e162eef725f9cbac51f8f543d15
ADSWT518 <tangyazhou518@outlook.com>
2021-10-30 23:44:02 +0800

    finish chapter3 hw ex2

--- chapter3-Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex2Activity.java
+++ chapter3-Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex2Activity.java
@@ -137,12 +137,31 @@ public class Ch3Ex2Activity extends AppCompatActivity {
         animator1.setRepeatMode(ObjectAnimator.REVERSE);
 
         // TODO ex2-1：在这里实现另一个 ObjectAnimator，对 target 控件的大小进行缩放，从 1 到 2 循环
-
+        ObjectAnimator animator2_1_1 = ObjectAnimator.ofFloat(target,
+                "scaleX",
+                1f,
+                2f);
+        animator2_1_1.setDuration(Integer.parseInt(durationSelector.getText().toString()));
+        animator2_1_1.setRepeatCount(ObjectAnimator.INFINITE);
+        animator2_1_1.setRepeatMode(ObjectAnimator.REVERSE);
+        ObjectAnimator animator2_1_2 = ObjectAnimator.ofFloat(target,
+                "scaleY",
+                1f,
+                2f);
+        animator2_1_2.setDuration(Integer.parseInt(durationSelector.getText().toString()));
+        animator2_1_2.setRepeatCount(ObjectAnimator.INFINITE);
+        animator2_1_2.setRepeatMode(ObjectAnimator.REVERSE);
         // TODO ex2-2：在这里实现另一个 ObjectAnimator，对 target 控件的透明度进行修改，从 1 到 0.5f 循环
-
+        ObjectAnimator animator2_2 = ObjectAnimator.ofFloat(target,
+                "alpha",
+                1f,
+                0.5f);
+        animator2_2.setDuration(Integer.parseInt(durationSelector.getText().toString()));
+        animator2_2.setRepeatCount(ObjectAnimator.INFINITE);
+        animator2_2.setRepeatMode(ObjectAnimator.REVERSE);
         // TODO ex2-3: 将上面创建的其他 ObjectAnimator 都添加到 AnimatorSet 中
         animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animator1);
+        animatorSet.playTogether(animator1, animator2_1_1, animator2_1_2, animator2_2);
         animatorSet.start();
     }
 }
